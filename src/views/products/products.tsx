e5516f4f8eb1e6bfadea4fe9e0b4895a76d0e7cc
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2024-10-30 08:24:10 +0600

    suspanse added

--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, Suspense } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -60,18 +60,20 @@ export const Products: React.FC = () => {
   }
 
   return (
-    <div>
-      <BackToHome />
-      <ProductList products={paginatedProducts} onOpenModal={handleOpenModal} />
-      <div className="h-4" />
-      <PaginationControls
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
-      {selectedProduct && (
-        <ProductModal product={selectedProduct} onClose={handleCloseModal} />
-      )}
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <BackToHome />
+        <ProductList products={paginatedProducts} onOpenModal={handleOpenModal} />
+        <div className="h-4" />
+        <PaginationControls
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+        {selectedProduct && (
+          <ProductModal product={selectedProduct} onClose={handleCloseModal} />
+        )}
+      </div>
+    </Suspense>
   );
 };
